1b400eddedb6140bddfd66f183fb89ac896c306b
Matty Kuhn <matty.kuhn.1@gmail.com>
2024-02-26 19:15:23 -0600

    add markdown preview

---
+++ lua/user/markdown.lua
@@ -0,0 +1,12 @@
+local M = {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
+
+M.config = function()
+    vim.g.mkdp_browser = "firefox"
+end
+
+return M
